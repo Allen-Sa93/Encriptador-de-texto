eaa011141c447958d5545b4a4cc2d3a76ba3252c
Allen Saborio <luals91@hotmail.com>
2024-03-19 23:53:48 -0600

    Agregando funcionabilidad para desencriptar

--- index.js
+++ index.js
@@ -26,3 +26,30 @@ function encriptar(){
     }
 
 }
+
+function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+        if(texto.length !=0){
+          document.getElementById("texto").value = textoCifrado;
+          tituloMensaje.textContent = "Texto desencriptado con éxito";           
+          parrafo.textContent = "";
+          muñeco.src = "./img/desencriptado.jpg"; 
+        } else {
+          muñeco.src = "./img/muñeco.png";
+          tituloMensaje.textContent = "Ningún texto ha sido cifrado";
+         parrafo.textContent = "Ingrese el texto que desea encriptar o desencriptar";
+         alert("Debes escribir algo para poder decifrarlor");
+        }
+
+}
